9f9c4b5da8f4b41ebf29bea740cdfb51a24aa629
Sandeep Singh <serperior2@gmail.com>
2021-03-18 00:02:03 -0400

    pages, blogs table-listing style

--- admin/src/views/blogs.php
+++ admin/src/views/blogs.php
@@ -2,22 +2,49 @@
 
 <h1>Blogs</h1>
 
-<a href="blog/editor">Create</a>
+<a href="blog/editor">New blog</a>
 
-<?php
-foreach ($this->blogs_arr as $blog) {
-    ?>
-    <div>
-        <a href="blog/editor?id=<?= $blog['id'] ?>">Edit <?php echo htmlentities($blog['title']) ?></a>
-        Last updated at: <?php echo htmlentities($blog['updated_at']) ?>
+<div id="blog-list-container">
+    <table id="blog-list-table">
+        <tr>
+            <th>Blog Title</th>
+            <th>Author</th>
+            <th>Tags</th>
+            <th>Created at</th>
+            <th>Updated at</th>
+            <th>Delete</th>
+        </tr>
+        
+        <?php foreach ($this->blogs_arr as $blog) : ?>
+        <tr>
+            <td class="blog-list">
+                <a href="blog/editor?id=<?= $blog['id'] ?>"><?= $this->escape($blog['title']) ?></a>
+            </td>
+            <td class="blog-list">
+                <span><?php $this->escape($blog['author']) ?></span>
+            </td>
+            <td class="blog-list">
+                <span><?= (implode(' | ', $blog['tags'])) ?></span>
+            </td>
+            <td class="blog-list">
+                <span><?= $blog['created_at'] ?></span>
+            </td>
+            <td class="blog-list">
+                <span><?= $blog['updated_at'] ?></span>
+            </td>
+                
+            <td class="grid-item">
+                <!-- delete -->
+                <form action="/blogs" method="post">
+                    <input type="hidden" name="delete">
+                    <input type="hidden" name="delete-id" value="<?= $blog['id'] ?>">
+    
+                    <input type="submit" value="X">
+                </form>
+            </td>
+        </tr>
+        <?php endforeach ?>
+    </table>
+</div>
 
-        <form action="/blogs" method="post">
-            <input type="hidden" name="delete">
-            <input type="hidden" name="delete-id" value="<?= $blog['id'] ?>">
-
-            <input type="submit" value="Delete">
-        </form>
-    </div>
-    <?php
-}
-?>
+<?php require __DIR__ . '/partials/footer.php' ?>
--- admin/src/views/pages.php
+++ admin/src/views/pages.php
@@ -1,33 +1,43 @@
 <?php require __DIR__ . '/partials/header.php' ?>
 
 <h1>Pages</h1>
-<h3>Fire Elf</h3>
 
 
 
-<a href="/pages/editor">Add a new page</a>
+<a href="/pages/editor">New page</a>
 
-<div id="pages_container">
-    <ol>
-        <?php
-        
-        foreach ($this->page_arr as $page) {
-            ?>
-            <li class="page">
-                <a href="/pages/editor?id=<?= $page['id'] ?>"><?php echo htmlentities($page['name']) ?></a>
-                Last updated at: <?php echo htmlentities($page['updated_at']) ?>
+
+<div id="page-list-container">
+    <table id="page-list-table">
+        <tr>
+            <th>Page Title</th>
+            <th>Created at</th>
+            <th>Updated at</th>
+            <th>Delete</th>
+        </tr>
+
+        <?php foreach ($this->page_arr as $page) : ?>
+        <tr>
+            <td class="page-list">
+                <a href="/pages/editor?id=<?= $page['id'] ?>"><?= $this->escape($page['name']) ?></a>
+            </td>
+            <td class="page-list">
+                <span><?= $page['created_at'] ?></span>
+            </td>
+            <td class="page-list">
+                <span><?= $page['updated_at'] ?></span>
+            </td>
+            <td class="page-list">
                 <form action="/pages" method="post">
                     <input type="hidden" name="delete">
                     <input type="hidden" name="delete-id" value="<?= $page['id'] ?>">
 
-                    <input type="submit" value="Delete">
+                    <input type="submit" value="X">
                 </form>
-            </li>
-            <?php
-        }
-
-        ?>
-    </ol>
+            </td>
+        </tr>
+        <?php endforeach ?>
+    </table>
 </div>
 
 <?php require __DIR__ . '/partials/footer.php' ?>
